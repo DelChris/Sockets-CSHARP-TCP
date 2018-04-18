8e92b698f1ad80a5f23f8772c4f13ac40aa9952e
TheSora06 <jeje.et.sora@gmail.com>
2018-04-18 14:24:30 +0200

    1 client à la fois et msg bidirectionnel

---
+++ 1a - WiFi Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _1a___WiFi_Client
+{
+    class Program
+    {
+        private static Socket _socket;
+        private static String input;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------ WiFi Client v0.1 ------------------");
+            Console.Write("Choisissez l'ip où vous connecter : ");
+            String address = Console.ReadLine();
+            Console.Write("Choisissez le port d'écoute : ");
+            int port = int.Parse(Console.ReadLine());
+            ClientTCP(IPAddress.Parse(address), port);
+        }
+
+        private static void ClientTCP(IPAddress address, int port)
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipEndPoint = new IPEndPoint(address, port);
+
+            _socket.Connect(ipEndPoint);
+            var thread = new Thread(Listen);
+            thread.Start();
+        }
+
+        private static void Listen()
+        {
+            while (true)
+            {
+                Console.WriteLine("Message à envoyer :");
+                input = Console.ReadLine();
+                _socket.Send(Encoding.UTF8.GetBytes(input));
+                Console.WriteLine("Message envoyé.");
+            }
+        }
+    }
+}
---
+++ 1b - WiFi Serveur/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _1b___WiFi_Serveur
+{
+    class Program
+    {
+        private static Socket _listenerSocket;
+
+        private static byte[] buffer;
+        private static int readBytes;
+
+        private static Socket SocketReceive;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------ WiFi Serveur v0.1 ------------------");
+            Console.WriteLine("Choisissez le port d'écoute : ");
+            int port = int.Parse(Console.ReadLine());
+            ServerTCP(port);
+        }
+
+        private static void ServerTCP(int port)
+        {
+            _listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var address = GetHostIPAdress();
+            IPEndPoint ip = new IPEndPoint(address, port);
+            _listenerSocket.Bind(ip);
+
+            var listenThread = new Thread(ListenThread);
+            listenThread.Start();
+            Console.WriteLine($"Success... Listening TCP : {address}:{port}");
+        }
+
+        private static void ListenThread()
+        {
+            while (true)
+            {
+                _listenerSocket.Listen(0);
+                //Clients.Add(new ClientManager(_listenerSocket.Accept()));
+                SocketReceive = _listenerSocket.Accept();
+                Console.WriteLine("Client connecté.");
+                buffer = new byte[SocketReceive.SendBufferSize];
+                readBytes = SocketReceive.Receive(buffer);
+                if(readBytes > 0)
+                {
+                    var msg = Encoding.UTF8.GetString(buffer, 0, readBytes);
+                    Console.WriteLine($"Client : {msg}");
+                }
+            }
+        }
+
+        private static IPAddress GetHostIPAdress()
+        {
+            return Dns.GetHostAddresses(Dns.GetHostName()).FirstOrDefault(_ => _.AddressFamily == AddressFamily.InterNetwork);
+        }
+    }
+}
